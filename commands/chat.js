a9e4008fbf5148ea1271221abe5220492ed7ed8e
LordOfTheCube <noahbilski11@gmail.com>
2020-10-31 23:37:26 +0000

    Removed logs from chat command

--- commands/chat.js
+++ commands/chat.js
@@ -1,7 +1,7 @@
 
 exports.run = async (client, message, args) => {
   if(!require('../checks/modcheck.js')(client, message)) {
-    console.log(require('../checks/modcheck.js')(client, message))
+    // console.log(require('../checks/modcheck.js')(client, message))
     return;
   }
 //console.log('user has mod')
